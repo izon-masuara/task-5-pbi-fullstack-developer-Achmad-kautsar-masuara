b97ce98a7fcf764df374de3e9723ada9bd3bb975
kautsar_masuara <kautsarmasuara@gmail.com>
2023-01-15 22:37:26 +0800

    implement env

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
 	github.com/gin-gonic/gin v1.8.2
 	github.com/golang-jwt/jwt/v4 v4.4.3
+	github.com/joho/godotenv v1.4.0
 	github.com/lib/pq v1.10.7
 	golang.org/x/crypto v0.4.0
 	gorm.io/driver/postgres v1.4.6
--- helpers/jwt.go
+++ helpers/jwt.go
@@ -1,6 +1,8 @@
 package helpers
 
 import (
+	"os"
+
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/izon-masuara/app"
 )
@@ -10,7 +12,7 @@ func GenerateToken(users app.User) (string, error) {
 		"UserID": users.ID,
 	})
 
-	resToken, err := token.SignedString([]byte("AccessToekn"))
+	resToken, err := token.SignedString([]byte(os.Getenv("JWT")))
 	if err != nil {
 		return "", err
 	}
@@ -19,7 +21,7 @@ func GenerateToken(users app.User) (string, error) {
 
 func VerifyToken(tokenString string) (jwt.Claims, error) {
 	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
-		return []byte("AccessToekn"), nil
+		return []byte(os.Getenv("JWT")), nil
 	})
 	if err != nil {
 		return nil, err
